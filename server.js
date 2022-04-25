30d8c67d41879254606f9114d43c8c0345a3d4de
CharanC1307 <66274903+CharanC1307@users.noreply.github.com>
2022-04-24 22:35:52 -0500

    Fix the send file index.html

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path')
 const socketApi = require('./socket.js')
 
 app = express()
-app.use(express.static(path.join(__dirname, 'app')))
 app.use('/favicon.ico', express.static(path.join(__dirname, 'favicon.ico')))
+app.use(express.static(path.join(__dirname, 'App')))
 
 PORT = process.env.PORT || 8080
 
